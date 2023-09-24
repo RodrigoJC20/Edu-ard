db886d2d21295c2691b673a04a84a3df9258c94b
MikelBarajas38 <mikelbarajas388@gmail.com>
2023-09-24 01:12:00 -0600

    async list now works

---
+++ app/firebaseutils.tsx
@@ -0,0 +1,34 @@
+import { db } from "../lib/firebase/index";
+import { addDoc, collection, doc, getDoc, onSnapshot } from "firebase/firestore";
+
+type Course = {
+    id: string;
+    name: string;
+    topics: string[];
+}
+
+export const addCourse = async (c: Course) => { // Fixing the parameter syntax here
+    await addDoc(collection(db, 'course'), {
+        name: c.name.trim(),
+        topics: c.topics
+    });
+}
+
+export const getDocRef = (courseId: string) => {
+    return doc(db, 'course', courseId);
+}
+
+export const getTopicList = async (courseId: string) => {   
+    const courseRef = getDocRef(courseId);
+    const docSnapshot = await getDoc(courseRef);
+    if (docSnapshot.exists()) {
+        const data = docSnapshot.data();
+        if (data) {
+            const topicsArray: string[] = data.topics || [];
+            return topicsArray;
+        }
+    } else {
+        console.log("Document does not exist");
+    }
+}
+
