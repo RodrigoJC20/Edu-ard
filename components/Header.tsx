e2222279bee2a889c7e967321814bd78ee9e1048
marcoantonnlopez <a305350@alumnos.uaslp.mx>
2023-09-24 01:46:33 -0600

    feat(instructor): create styles

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import styles from './Header.module.css';
+
+export default function Header() {
+    return (
+        <header className="text-white p-4">
+            <div className={styles.ej}>
+                <div className={styles.ej2}>
+                    <img className={styles.logo} src="/logo.svg" alt="" />
+                    <img className={styles.logo2} src="/Edu-ard.svg" alt="" />
+                </div>
+                <nav>
+                    <ul className="flex space-x-4">
+                        <li><a href="#" className="hover:underline">Log out</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
