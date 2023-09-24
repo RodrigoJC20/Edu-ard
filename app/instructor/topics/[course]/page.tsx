a8949770f2f0237c1848d1e5aa91f20a0f8152f1
MisterChief53 <soderol3@hotmail.com>
2023-09-24 05:47:02 -0600

    prettier submit topic page

--- app/instructor/topics/[course]/page.tsx
+++ app/instructor/topics/[course]/page.tsx
@@ -1,13 +1,21 @@
-'use client'
+"use client"
 
 import { useState, useEffect } from "react";
-import { collection, addDoc, onSnapshot, query, doc, setDoc, getDoc, updateDoc } from "firebase/firestore"; 
+import {
+    collection,
+    addDoc,
+    onSnapshot,
+    query,
+    doc,
+    setDoc,
+    getDoc,
+    updateDoc,
+} from "firebase/firestore";
 import { db } from "../../../../lib/firebase/index";
 import { getDocRef } from "@/app/firebaseutils";
-import styles  from './topics.module.css';
+import styles from './topics.module.css';
 
-export default function TopicList({ params } : {params: {course: string}}) {
-
+export default function TopicList({ params }: { params: { course: string } }) {
     const [topics, setTopics] = useState<string[]>([]);
     const [courseName, setCourseName] = useState<string>('');
     const [newTopic, setTopic] = useState<string>('');
@@ -33,7 +41,7 @@ export default function TopicList({ params } : {params: {course: string}}) {
                 if (data) {
                     // Assuming there's a "topics" field in your document
                     const topicsArray: string[] = data.topics || [];
-                    
+
                     setTopics(topicsArray);
                     setCourseName(data.name);
                 }
@@ -47,25 +55,42 @@ export default function TopicList({ params } : {params: {course: string}}) {
     }, [params.course]);
 
     return (
-        <div className={styles.main}>
-            <form onSubmit={addTopic} className={styles.form}>
-                <h3>{courseName}</h3>
-                <input
-                    type="text"
-                    placeholder="Nombre del curso"
-                    value={newTopic}
-                    onChange={(e) => setTopic(e.target.value)}
-                />
-                <button type="submit">Agregar</button>
-            </form>
-            <div>
-                <div className={styles.topic}>
-                    {topics?.map((topic, index) => (
-                        <div key={index}>
-                            <h1>{topic}</h1>
-                            <button>X</button>
-                        </div>
-                    ))}
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+                <form onSubmit={addTopic} className="mb-4">
+                    <h3 className="text-xl font-semibold mb-2">{courseName}</h3>
+                    <div className="flex items-center space-x-2"> {/* Centered items */}
+                        <input
+                            type="text"
+                            placeholder="Nombre del curso"
+                            value={newTopic}
+                            onChange={(e) => setTopic(e.target.value)}
+                            className="flex-1 rounded-md border border-gray-300 px-3 py-2 outline-none focus:border-blue-500" // Added "flex-1" for width
+                        />
+                        <button
+                            type="submit"
+                            className="bg-purple-500 text-white px-4 py-2 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600" // Changed button color to purple
+                        >
+                            Agregar
+                        </button>
+                    </div>
+                </form>
+                <div>
+                    <div className="space-y-2">
+                        {topics?.map((topic, index) => (
+                            <div
+                                key={index}
+                                className="bg-white p-4 flex items-center justify-between shadow-md rounded-md"
+                            >
+                                <h1 className="text-lg font-semibold">{topic}</h1>
+                                <button
+                                    className="text-red-600 hover:text-red-800 focus:outline-none"
+                                >
+                                    X
+                                </button>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
@@ -75,7 +100,7 @@ export default function TopicList({ params } : {params: {course: string}}) {
 function Topic({ topic }: any) {
     return (
         <div>
-        <p>{topic}</p>
+            <p>{topic}</p>
         </div>
     );
 }
