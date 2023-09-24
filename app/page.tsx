276d8f93227a2ed46ea509592846a7ad080ceb54
marcoantonnlopez <a305350@alumnos.uaslp.mx>
2023-09-23 22:10:57 -0600

    feat(landingPage):add landing page style

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,40 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import styles from './page.module.css';
 
 export default function Home() {
   const [course, setCourse] = useState('');
   const router = useRouter();
 
-  const launchChat = async(e : any) => {
+  const launchChat = async (e: any) => {
     e.preventDefault();
     router.push(`/student/${course}`);
   }
 
   return (
-    <main>
-      <h1>Hello, World!</h1>
-      <form onSubmit={launchChat}>
-          <h3>Curso</h3>
+    <main className={styles.main}>
+      <div>
+        <img className={styles.logo} src="/logoEdu1.gif" alt="logo" />
+      </div>
+
+      <section className={styles.info}>
+
+        <img src="/Edu-ard.svg" alt="logo" />
+          <h3>Join an Edu-ard chatbot course with a link</h3>
+        <form className={styles.form} onSubmit={launchChat}>
           <input
-              type="text"
-              placeholder="Tema"
-              value={course}
-              onChange={(e) => setCourse(e.target.value)}
+          className={styles.input}
+            type="text"
+            placeholder="Paste a link in here.."
+            value={course}
+            onChange={(e) => setCourse(e.target.value)}
           />
-          <button type="submit">Ir</button>
-      </form>
-      <Link href="/instructor/">Soy maestro</Link>
+          <button type="submit">Go</button>
+        </form>
+        <Link href="/instructor/">I’m a professor</Link>
+      </section>
+
     </main>
   )
 }
